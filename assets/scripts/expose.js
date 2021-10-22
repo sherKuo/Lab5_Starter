c78dfbb83316aed4edddf64c2dadb74349e83765
sherKuo <sherilynkuo@gmail.com>
2021-10-22 14:38:56 -0700

    testing airhorn

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,25 @@
 // expose.js
+var selectedHornImage = document.getElementById("horn-select");
+var
 
-window.addEventListener('DOMContentLoaded', init);
+
+//window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var expr = document.getElementById("horn-select");
+  switch (expr) {
+    case 'air-horn':
+      expr.src = "assets/images/air-horn.svg";
+      console
+      break;
+    case 'car-horn':
+    case 'party-horn':
+      console.log('Mangoes and papayas are $2.79 a pound.');
+      // expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+  }
+
 }
