11034a3a9e67f40e25dd272758560aa40a135bcf
sherKuo <sherilynkuo@gmail.com>
2021-10-23 16:18:45 -0700

    Uploading

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,19 +1,33 @@
 // explore.js
 window.addEventListener('DOMContentLoaded', init);
 
-var text = document.querySelector("textarea");
-var 
+var inText = document.querySelector("textarea").value;
+var voiceSelector = document.querySelector()
 
+var synth = window.speechSynthesis;
 
 var click = document.querySelector("button")
-click.addEventListener("click", speakText);
+click.addEventListener("click", init);
 
 function init() {
   // TODO
-  let utterance = new SpeechSynthesisUtterance(text);
+  let utterance = new SpeechSynthesisUtterance(inText);
   speechSynthesis.speak(utterance);
 }
 
-function speakText(){
-  //similar to sound.play but for this version. 
+function populateVoices() {
+  voices = synth.getVoices();
+
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
 }
