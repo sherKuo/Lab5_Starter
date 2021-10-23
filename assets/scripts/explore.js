e09515600f4022cfcfa23a0dbb3a1ace931edc26
sherKuo <sherilynkuo@gmail.com>
2021-10-23 16:16:33 -0700

    pushing to merge

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,19 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
+
+var text = document.querySelector("textarea");
+var 
+
+
+var click = document.querySelector("button")
+click.addEventListener("click", speakText);
 
 function init() {
   // TODO
+  let utterance = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(utterance);
+}
+
+function speakText(){
+  //similar to sound.play but for this version. 
 }
